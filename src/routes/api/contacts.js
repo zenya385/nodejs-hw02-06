64b4f0f76b5a6b5eaf0566042e680ef58411250c
Roman Zeno <zeno385@gmail.com>
2022-05-02 18:50:44 +0300

    create

--- src/routes/api/contacts.js
+++ src/routes/api/contacts.js
@@ -2,7 +2,7 @@ const express = require("express");
 const {
   addPostValidation,
   patchValidation,
-} = require("../../middlewares/validationSchema");
+} = require("../../middleware/validationSchema");
 const router = express.Router();
 const {
   listContacts,
@@ -33,11 +33,46 @@ router.post("/", addPostValidation, async (req, res, next) => {
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const { contactId } = req.params;
+  const contactById = await getContactById(contactId);
+  if (!contactById) {
+    return res.status(404).json({ status: "Not found" });
+  } else {
+    await removeContact(contactId);
+    res.status(200).json({ message: "contact deleted" });
+  }
 });
 
-router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+router.put("/:contactId", addPostValidation, async (req, res) => {
+  const { contactId } = req.params;
+  const { name, email, phone } = req.body;
+  const contactById = await getContactById(contactId);
+  if (!contactById) {
+    return res.status(404).json({ status: "Not found" });
+  } else {
+    const updateContactItem = await updateContact(contactId, {
+      name,
+      email,
+      phone,
+    });
+    res.status(200).json({ status: "success", updateContactItem });
+  }
+});
+
+router.patch("/:contactId", patchValidation, async (req, res) => {
+  const { contactId } = req.params;
+  const { name, email, phone } = req.body;
+  const contactById = await getContactById(contactId);
+  if (!contactById) {
+    return res.status(404).json({ status: "Not found" });
+  } else {
+    const updateContactItem = await updateContact(contactId, {
+      name,
+      email,
+      phone,
+    });
+    res.status(200).json({ status: "success", updateContactItem });
+  }
 });
 
 module.exports = router;
